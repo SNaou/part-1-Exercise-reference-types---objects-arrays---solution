b715ec57bb210e735ff3109ce53e3603857c887e
SNaou <S.Naou@icloud.com>
2023-01-16 12:42:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let person = {
+    name: "Soumaya",
+    age: 37
+  };
+  
+
+  console.log(person.name);
+  
+  console.log(person.evaluations); // Output: [7, 10, 9]
+
+
+  /*bracket notation:
+
+  let person = {
+    name: "Soumaya",
+    age: 37
+  };
+  console.log(person["age"]);
+
+console.log(person["evaluations"]); // Output: [7, 10, 9]
+
+*/
+
